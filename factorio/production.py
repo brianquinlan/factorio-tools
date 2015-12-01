c756c7f218b55e82c6bc9137e6ec4d2944d6f143
Brian Quinlan <brian@sweetapp.com>
2015-12-01 12:03:35 -0800

    Added more production machines. Style fixes.

--- factorio/production.py
+++ factorio/production.py
@@ -1,19 +1,35 @@
-
-"""
-crafting speeds:
-../entity/entities.lua
-"""
+"""Data and computations related to production machines."""
 
 class Producer(object):
+    """A machine that can produce items."""
     crafting_category_to_producers = {}
 
     def __init__(self, name, crafting_categories, crafting_speed):
+        """Initialize the producer.
+
+        Args:
+            name: The name of the producer e.g. 'electric-furnace'.
+            crafting_categories: A sequence of crafting categories that the
+                producer supports e.g. ['chemisty', 'smelting'].
+            crafting_speed: The producers crafting rate e.g. 1.5.
+        """
         self.name = name
-        self.crafting_categories = crafting_categories
+        self.crafting_categories = frozenset(crafting_categories)
         self.crafting_speed = crafting_speed
 
     @classmethod
     def get_most_efficient_producer(cls, category):
+        """Returns the most efficient Producer for a crafting category.
+
+        Args:
+            category: The crafting category e.g. 'chemisty' or 'smelting'.
+
+        Returns:
+            A producer that can produce that crafting category.
+
+        Raises:
+            KeyError: if no Producer can produce the given crafting category.
+        """
         return max(cls.crafting_category_to_producers[category],
                    key=lambda p: p.crafting_speed)
 
@@ -25,15 +41,24 @@ class Producer(object):
 
     @classmethod
     def init(cls):
+        """Load the producer information.
+
+        A subset of the data taken from:
+            - base/prototypes/entities/entities.lua
+            - base/prototypes/entities/demo-entities.lua
+        """
         cls._add_producer(Producer('electric-furnace', {'smelting'}, 2))
+        cls._add_producer(Producer('assembling-machine-1', {'crafting'}, 0.5))
+        cls._add_producer(
+            Producer(
+                'assembling-machine-2',
+                {'crafting', 'advanced-crafting', 'crafting-with-fluid'}, 0.75))
         cls._add_producer(
             Producer(
                 'assembling-machine-3',
-                {"crafting", "advanced-crafting", "crafting-with-fluid"}, 1.25))
-        cls._add_producer(
-            Producer(
-                'chemical-plant',
-                {"chemistry"}, 1.25))
+                {'crafting', 'advanced-crafting', 'crafting-with-fluid'}, 1.25))
+        cls._add_producer(Producer('chemical-plant', {'chemistry'}, 1.25))
+        cls._add_producer(Producer('oil-refinery', {'oil-processing'}, 1))
 
 
 Producer.init()
--- factorio/tests/production_test.py
+++ factorio/tests/production_test.py
@@ -1,19 +1,19 @@
-import os.path
+"""Tests for factorio.production."""
+
 import unittest
 
 from factorio import production
 
 class TestProduction(unittest.TestCase):
 
-
     def test_get_most_efficient_crafting_machine(self):
-        assembling_machine_3 = production.Producer.get_most_efficient_producer('crafting')
+        assembling_machine_3 = production.Producer.get_most_efficient_producer(
+            'crafting')
         self.assertEqual(assembling_machine_3.name, 'assembling-machine-3')
         self.assertEqual(
             assembling_machine_3.crafting_categories,
             {'crafting', 'crafting-with-fluid', 'advanced-crafting'})
         self.assertEqual(assembling_machine_3.crafting_speed, 1.25)
-
 
 if __name__ == '__main__':
     unittest.main()
